44b3b46969870c2f216de3d807d8dbcdde7db2c9
Pathini Ekanayaka <pathini2002@gmail.com>
2025-04-19 13:45:39 +0530

    update shadow blob

--- src/app/pages/contact/page.jsx
+++ src/app/pages/contact/page.jsx
@@ -6,36 +6,35 @@ import LocationSection from "../../components/contact-us/LocationSection";
 
 export default function Page() {
   return (
-    <div className="flex flex-col min-h-screen relative overflow-x-hidden">
+    <div className="flex flex-col min-h-screen relative">
+      <Image
+        src="/images/contact-us-page/Ellipse 8.png"
+        alt="Form background shadow"
+        width={800}
+        height={800}
+        className="absolute -top-50 right-0"
+      />
+
+      <Image
+        src="/images/contact-us-page/Ellipse 8.png"
+        alt="Form background shadow"
+        width={600}
+        height={600}
+        className="rotate-180 absolute left-0 top-60"
+        priority
+      />
+
+      <Image
+        src="/images/contact-us-page/Ellipse 8.png"
+        alt="Form background shadow"
+        width={500}
+        height={500}
+        className="absolute bottom-50 right-0"
+      />
       <ContactUsHero />
-      <div className="absolute -left-44 2xl:-left-200 top-[730px]  2xl:w-[2000px] 2xl:h-[1000px] w-[800px] h-[800px] lg:z-10 2xl:z-0 rotate-180">
-                  <Image
-                    src="/images/contact-us-page/Ellipse 8.png"
-                    alt="Form background shadow"
-                    fill
-                    className="object-contain"
-                    priority
-                  />
-                </div>
-                <div className="absolute -right-35 top-[50px] w-[850px] h-[1000px] z-10">
-                  <Image
-                    src="/images/contact-us-page/Ellipse 8.png"
-                    alt="Form background shadow"
-                    fill
-                    className="object-contain"
-                    priority
-                  />
-                </div>
-                <div className="absolute -right-38 xl:-right-65 bottom-[400px] w-[600px] h-[650px] 2xl:w-[800px] 2xl:h-[800px] z-10">
-                  <Image
-                    src="/images/contact-us-page/Ellipse 8.png"
-                    alt="Form background shadow"
-                    fill
-                    className="object-contain"
-                    priority
-                  />
-                </div> 
-      <LocationSection />
+      <div className="relative z-20">
+        <LocationSection />
+      </div>
     </div>
   );
 }
