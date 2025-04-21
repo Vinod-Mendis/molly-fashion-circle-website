fd1fd123fb3248b83706738aa61e75c848d3d167
Pathini Ekanayaka <pathini2002@gmail.com>
2025-04-21 15:25:20 +0530

    optimize the code for hovering effect in address section

---
+++ src/app/components/contact-us/Addressblock.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { FaMapMarkerAlt } from "react-icons/fa";
+import AddressItem from "./AddressItem";
+
+export default function Addressblock({ branchNumber, no, street, town }) {
+  return (
+    <div>
+      {/* Branch Header */}
+      <div className="flex items-center mb-2 text-[#CDCDCD]">
+        <FaMapMarkerAlt className="mr-2" />
+        <span className="text-xs uppercase">branch {branchNumber}</span>
+      </div>
+
+      {/* Address Block */}
+      <div className="text-black">
+        {/* First Line: Number and Street */}
+        <div className="flex flex-wrap">
+          <AddressItem text={no} dialogueText="Number" />
+          <AddressItem text={street} dialogueText="Street" />
+        </div>
+
+        {/* Town */}
+        <div className="flex flex-wrap mt-1">
+          <AddressItem text={town} dialogueText="Town" />
+        </div>
+
+        {/* Country (hardcoded as mentioned in requirements) */}
+        <div className="flex flex-wrap mt-1">
+          <AddressItem text="Sri Lanka" dialogueText="Country" comma={false} />
+        </div>
+      </div>
+    </div>
+  );
+}
