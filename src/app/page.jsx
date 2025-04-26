a573f478cc67637903a96a8632c128634920c469
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-04-26 17:16:35 +0700

    feat: redirect home page to coming soon page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,10 @@ import Hero from "./components/home/Hero";
 import LatestArrivals from "./components/home/LatestArrivals";
 import StylesSection from "./components/home/StylesSection";
 import TimeDeals from "./components/home/TimeDeals";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/pages/coming-soon");
   return (
     <div className="flex flex-col min-h-screen gap-20">
       <Hero />
