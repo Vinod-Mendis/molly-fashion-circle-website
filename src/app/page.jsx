05a4c51c19e44388806a19045d28f08061bd841d
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-04-27 20:04:55 +0700

    feat: add ProductCarousel and ProductCarouselCard components; update LatestArrivals to include mobile carousel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,12 +8,12 @@ import TimeDeals from "./components/home/TimeDeals";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-[100svh] gap-20 overflow-hidden">
-        <ComingSoonComponent />
-      {/* <Hero />
+    <div className="flex flex-col gap-20 overflow-hidden">
+        {/* <ComingSoonComponent /> */}
+      <Hero />
       <StylesSection />
       <LatestArrivals />
-      <TimeDeals /> */}
+      <TimeDeals />
     </div>
   );
 }
